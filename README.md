0ffeee6bd6f9975be957ae8e204ea13f25356fda
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2023-04-03 14:24:08 +0200

    Complete example using DD4hep

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-To compile,
+Source the paths, for example, from cvmfs
 
 ```shell
 source /cvmfs/sft.cern.ch/lcg/views/dev4/latest/x86_64-centos7-gcc11-opt/setup.sh 
+```
+
+To compile,
+```shell
 cmake -B build -S . -D CMAKE_INSTALL_PREFIX=install
 cmake --build build -- install
+```
+
+After that, the install directory will look like this:
+
+```shell
+$ tree install/
+install/
+└── lib
+    ├── libsimple_example.components
+    └── libsimple_example.so
+
+1 directory, 2 files
+```
+
+To let the programs know where to find our freshly built detector, we have to update this env variable
+
+```shell
 export LD_LIBRARY_PATH=$PWD/install/lib:$LD_LIBRARY_PATH
 ```
 
-To run one alpha particle of 1GeV in a brick of lead,
+To run the simulation of 1GeV alpha particle we can execute this:
 
 ```shell
-ddsim --compactFile compact/simple_detector.xlm --runType qt --enableGun --gun.particle alpha --gun.energy 1*GeV  -N 1 --outputFile kk2_edm4hep.root --macroFile vis.mac --gun.distribution uniform --gun.isotrop true
+ddsim --compactFile compact/simple_detector.xml --runType qt --enableGun --gun.particle alpha --gun.energy 1*GeV  -N 1 --outputFile example_edm4hep.root --macroFile vis.mac --gun.distribution uniform --gun.isotrop true
 ```
 
 The track can be something like ![A track of 10GeV alpha in lead](https://mattermost.web.cern.ch/files/f1tnt4n4n7dabk7zpbnwxbyxdc/public?h=usG4tmiWGuhWBAujICrw-K5bv63s6TR0izjSpG4CvjM)
@@ -18,7 +39,7 @@ The track can be something like ![A track of 10GeV alpha in lead](https://matter
 If we inspect the output rootfile,
 
 ```shell
-root kk2_edm4hep.root
+root example_edm4hep.root
 ```
 
 There is only one entry (1 event), so we can show the contents
--- src/simple_detector.cpp
+++ src/simple_detector.cpp
@@ -5,57 +5,53 @@
 #include <XML/Helper.h>
 
 using namespace dd4hep;
-using namespace dd4hep::rec;
-using dd4hep::SubtractionSolid;
 
 // create the detector
 static Ref_t createDetector(Detector &desc, xml::Handle_t handle, SensitiveDetector sens)
 {
   xml::DetElement detElem = handle;
+
+  // Get detector name and ID from compact file
   std::string detName = detElem.nameStr();
   int detID = detElem.id();
-  DetElement det(detName, detID);
-  sens.setType("calorimeter");
+
+  // Get z position from compact file
   auto zpos = detElem.attr<double>(_Unicode(zpos));
 
-  // We define our own mother volume (vacuum)
-  // that will be inside of world (made of air)
-  Box motherSolid(20 * cm / 2.,
-                  20 * cm / 2.,
-                  20 * cm / 2.);
-  Volume motherVol("myworld", motherSolid, desc.material("Vacuum"));
-  motherVol.setVisAttributes(desc.visAttributes("no_vis"));
+  // Setup which kind of sensitive detector is
+  sens.setType("calorimeter");
 
-
+  // Create the mother Detector element to be returned at the end
+  DetElement det(detName, detID);
 
   // How to define one detector:
-  {
-    // Define geometrical shape
-    Box siSolid(19 * cm / 2.,
-                19 * cm / 2.,
-                19 * cm / 2.);
-    // Define logical volume (geometry+material)
-    Volume siVol(detName + "_R", siSolid, desc.material("Lead"));
-      siVol.setVisAttributes(desc.visAttributes("sensor_vis"));
-      siVol.setSensitiveDetector(sens);
-      siVol.setLimitSet(desc, detElem.limitsStr());
+  // Define geometrical shape
+  Box siSolid(19 * cm / 2.,
+              19 * cm / 2.,
+              19 * cm / 2.);
+  // Define volume (shape+material)
+  Volume siVol(detName +"_sensor", siSolid, desc.material("Silicon"));
+  siVol.setVisAttributes(desc.visAttributes("sensor_vis"));
+  siVol.setSensitiveDetector(sens);
+  siVol.setLimitSet(desc, detElem.limitsStr());
 
-    // Place it inside the mother volume
-    PlacedVolume siPV = motherVol.placeVolume(siVol, Position(0, 0, zpos));
-    
-    // Add bitfield module to this particular detector
-    siPV.addPhysVolID("module", 0);
-  }
 
   // Place our mother volume in the world
   Volume wVol = desc.pickMotherVolume(det);
-  PlacedVolume mPV = wVol.placeVolume(motherVol, Position(0, 0, 0));
+
+  // setup mother volume as transparent.
+  // This is just for example, bad practice in general!
+  wVol.setVisAttributes(desc.visAttributes("no_vis"));
+
+
+  PlacedVolume siPV = wVol.placeVolume(siVol, Position(0, 0, zpos));
 
   // Assign the system ID to our mother volume
-  mPV.addPhysVolID("system", detID);
+  siPV.addPhysVolID("system", detID);
 
-  // Set the physical volumes of the detector element. 
-  det.setPlacement(mPV);
+  // Associate the silicon Placed Volume to the detector element.
+  det.setPlacement(siPV);
+
   return det;
 }
 
