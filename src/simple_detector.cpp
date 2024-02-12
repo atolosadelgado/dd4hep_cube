18ae60d94f888842b596aa375794ea535a643065
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2024-02-12 08:30:33 +0100

    take units from dd4hep

--- src/simple_detector.cpp
+++ src/simple_detector.cpp
@@ -21,11 +21,11 @@ static Ref_t createDetector(Detector &desc, xml::Handle_t handle, SensitiveDetec
   // Create the mother Detector element to be returned at the end
   DetElement det(detName, detID);
 
-  double twist_angle = 180*deg;
-  double rmin = 5*cm;
-  double rmax = 10*cm;
-  double dz = 10*cm;
-  double dphi = 90*deg;
+  double twist_angle = 180*dd4hep::deg;
+  double rmin = 5*dd4hep::cm;
+  double rmax = 10*dd4hep::cm;
+  double dz = 10*dd4hep::cm;
+  double dphi = 90*dd4hep::deg;
   TwistedTube myshape( twist_angle,  rmin,  rmax,  dz,  dphi);
   // Define volume (shape+material)
   Volume siVol(detName +"_sensor", myshape, desc.material("Silicon"));
