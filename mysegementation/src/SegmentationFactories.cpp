69f28632aff098f6d581194bfe078d68875e3abd
Alvaro Tolosa Delgado <alvaro.tolosa.delgado@cern.ch>
2024-10-31 14:50:10 +0100

    add custom segmentation, taken from k4geo

---
+++ mysegementation/src/SegmentationFactories.cpp
@@ -0,0 +1,13 @@
+#include "DD4hep/Factories.h"
+#include "DD4hep/detail/SegmentationsInterna.h"
+
+namespace {
+template <typename T>
+dd4hep::SegmentationObject* create_segmentation(const dd4hep::BitFieldCoder* decoder) {
+  return new dd4hep::SegmentationWrapper<T>(decoder);
+}
+}
+
+#include "GridTheta_k4geo.h"
+DECLARE_SEGMENTATION(mygrid, create_segmentation<dd4hep::DDSegmentation::GridTheta_k4geo>)
+
